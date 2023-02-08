7616368da466adf75c01bebc0dd9361ec8477fbc
john <alfred_alona@yahoo.com>
2023-02-08 18:30:40 +0000

    I am working perfectly fine w/o greeting

--- helloworld.sh
+++ helloworld.sh
@@ -2,13 +2,11 @@
 
 # Script: Ops 201 Class 02 Ops Challenge Solution
 # Author: John Alona
-# Date of latest revision: "02/07/2023"
+# Date of latest revision: "02/08/2023"
 # Purpose: Print a string to the terminal
 
 
 # Main
-greeting='Hello world!'
 name="I am working perfectly fine"
 echo "$name"
-
 # End
