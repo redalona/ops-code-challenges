fb2fb505f0c97a78f05a11a581f7ce836377d328
john <alfred_alona@yahoo.com>
2023-02-08 00:02:24 +0000

    1st helloworld.sh commit

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: John Alona
+# Date of latest revision: "02/07/2023"
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+pwd
+whoami
+ip a
+sudo apt update
+
+# End
