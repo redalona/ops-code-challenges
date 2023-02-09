195610a9aec26591878c0aaac01bac2e63680f5c
john <alfred_alona@yahoo.com>
2023-02-09 17:52:07 +0000

    /02/09/2023 Revision

--- Lab03.sh
+++ Lab03.sh
@@ -5,12 +5,11 @@
 # Date of latest revision: "02/08/2023"
 # Purpose: Write a script that prints the login history of users on this computer
 
-last
 function LOG_IN_HISTORY {
-LogInHistory="$(hostname)"
-initiate="$last"
+LogInHistory=$(hostname)
+initiate=$(last)
 
-if [ "$loginhistory" == "$hostname" ]; then
+if [ "$LogInHistory" == "ujohn" ]; then
     echo "$initiate"
 fi
 }
