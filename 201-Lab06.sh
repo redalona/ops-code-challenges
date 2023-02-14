57adf34dbad8afd975badfbdd43ed3c71203fcc6
john <alfred_alona@yahoo.com>
2023-02-14 04:48:31 +0000

    second commit

--- 201-Lab06.sh
+++ 201-Lab06.sh
@@ -15,33 +15,47 @@ function mkdir1 (){
 
 count0=1
 while [[ $count0 -eq 1 ]]; do
-    echo "do you want to create a new directory?"
+    echo "do you want to create new directories?yes or no"
     read array
         if [[ $array == "yes" ]]; then
             mkdir1
             count0=1
         elif [[ $array == "no" ]]; then
-            echo "do you want to create a new file?"
-            count0=2
-        else echo "Hello?"
+            count0=0
+        else echo "Hello? Wrong input!"
+            count0=1
         fi
 done
 
 #creating text file
-function touch1 (){
+
+function toucharray () {
+array1=("./dir1/" "./dir2/" "./dir3/" "./dir4/")   
     touch "${array1[0]}file.txt"
     touch "${array1[1]}file.txt"
     touch "${array1[2]}file.txt"
     touch "${array1[3]}file.txt"
 }
+
 count1=1
+
 while [[ $count1 -eq 1 ]]; do
+    
+    echo "do you want to create new files?yes or no"
     read touch
+    
         if [[ $touch == "yes" ]]; then
-        touch1
+        toucharray
+        count1=0
+        echo Done!
         elif [[ $touch == "no" ]]; then
-        echo "terminating"
-        count0=2
-        else echo "come on!"
+        echo "terminating......."
+        echo "Done!"
+        count1=0
+        else echo "You are better than that!"
+        count1=1
         fi
 done
+
+
+
