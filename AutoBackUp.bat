8c90f26b6e1f8b9c5a2d3b2a48e70bc9618a8dab
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-02-15 19:05:18 -0800

    Delete AutoBackUp.bat

--- AutoBackUp.bat
+++
@@ -1,5 +0,0 @@
-robocopy "C:\Users\j.thompson\Desktop\My Project Files" "E:\My Project Files Backup" projectfiles.txt
-robocopy "C:\Users\j.thompson\Desktop\My Project Files" "E:\My Project Files Backup" "projectfiles - Copy.txt"
-robocopy "C:\Users\j.thompson\Desktop\My Project Files" "E:\My Project Files Backup" "projectfiles - Copy (2).txt"
-robocopy "C:\Users\j.thompson\Desktop\My Project Files" "E:\My Project Files Backup" "projectfiles - Copy (3).txt"
-timeout 100
