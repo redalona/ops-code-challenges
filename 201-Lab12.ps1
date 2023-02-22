f6b08d1122199a515b2674961cb5cac29eb86573
John <alfred_alona@yahoo.com>
2023-02-22 18:54:19 +0000

    second commit

--- 201-Lab12.ps1
+++ 201-Lab12.ps1
@@ -4,12 +4,12 @@
 # Purpose: Create a Powershell script that performs the following operations:
 
 # Create a local file called network_report.txt that holds the contents of an ipconfig /all command.
-netrep = ipconfig /all >network_report.txt
-$netrep
+ipconfig /all >network_report.txt
+
 # Use Select-String to search network_report.txt and return only the IP version 4 address.
 function net_rep {
     Select-String -Path .\network_report.txt -Pattern "IPv4" |
 }
-$net_rep
+net_rep
 # Remove the network_report.txt when you are finished searching it.
-Remove-Item -Path C:\Users\john\Desktop\network_report.txt
+Remove-Item -Path .\network_report.txt
