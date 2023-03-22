44341000dfe454f999835635a0cd574ebda8b06e
john <alfred_alona@yahoo.com>
2023-03-22 23:20:41 +0000

    Complete no stretch

---
+++ 301d6-challenge08.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: John Alfred C. Alona
+# Date of latest revision: "03/22/2023"
+# Purpose:
+
+#Main
+# Assign to a variable a list of ten string elements.
+Str10 = ["Aardvark", "Babirusa", "Cabbage Moth", "Dachsador", "Eagle", "Fainting Goat", "Gaboon Viper", "Haast's Eagle", "Ibex", "Jabiru"]
+# Print the fourth element of the list.
+print(Str10[3])
+# Print the sixth through tenth element of the list.
+print(Str10[5:9])
+# Change the value of the seventh element to “onion”.
+Str10[6] = "onion"
+print(Str10[6])
+#End
+
+
+# Stretch Goals (1 Tuple, 1 set, 1 Dictionary)
+# print(Str10[6])
+# # append()
+# Str10.append("Kagu")
+# print(Str10)
+# # copy()
+# new_var = Str10.copy()
+# print(new_var)
+# new_var.append("Labahoula")
+# print(new_var)
+
+# # count()
+# # extend()
+# # index()
+# # insert()
+# # pop()
+# # remove()
+# # reverse()
+# # sort()
+# # clear()
+# Str10.clear()
+# print(Str10)
