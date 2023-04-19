eca1a1f4c0d39fdd9dea1443c3c2da5356046932
john <alfred_alona@yahoo.com>
2023-04-19 03:43:50 +0000

    second commit

--- 401d6-challenge02.py
+++ 401d6-challenge02.py
@@ -13,17 +13,19 @@ from datetime import datetime
 #For every ICMP transmission attempted, print the status variable along with a comprehensive timestamp and destination IP tested.
 #Example output: 2020-10-05 17:57:57.510261 Network Active to 8.8.8.8
 
+
 pingwhat = input("Enter Ping Address: ")
-ping = os.system("ping -c 1 " + pingwhat)
-if ping == 0:
-    print(ping)
-    print("Ping Successful")
-    ctime = datetime.now()
-    print(ctime)
-else:
-    while True:
-        print("Ping Unsuccessful")
-        print(ping)
+while True:
+    ping = os.system("ping -c 1 " + pingwhat)
+    if ping == 0:
         time.sleep(2)
+        print(ping)
+        print("Ping Successful")
+        ctime = datetime.now()
+        print(ctime)
+    else:
+        time.sleep(2)
+        print(ping)
+        print("Ping Unsuccessful")
         ctime = datetime.now()
         print(ctime)
